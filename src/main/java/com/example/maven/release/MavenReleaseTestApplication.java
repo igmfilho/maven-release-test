f89bc426685db558cca107c7201f254b953e356b
Ivan Filho <igmfilho@gmail.com>
2023-10-29 21:45:24 +0000

    Initial commit

---
+++ src/main/java/com/example/maven/release/MavenReleaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReleaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maven/release/MavenReleaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenReleaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
